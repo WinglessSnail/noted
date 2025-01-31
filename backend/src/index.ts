bc84ed94def4222255bd91a762c55cd1634c66e7
winglesssnail <hashcalabell@gmail.com>
2025-01-31 21:37:03 +0330

    initial

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("API is already running...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
